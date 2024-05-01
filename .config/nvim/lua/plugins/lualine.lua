e515fd5d12ac39333675c6e0b72b43f1db23772b
arghpy <arghpy@t420>
2024-05-01 19:21:05 +0300

    updated with the latest configurations

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,6 +3,14 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
 
     config = function()
-        require('lualine').setup()
+        require('lualine').setup{
+            sections = {
+                lualine_a = {'mode'},
+                lualine_b = {'filename'},
+                lualine_c = {},
+                lualine_x = {'filetype'},
+                lualine_y = {'progress'},
+                lualine_z = {'location'}
+            },}
     end
 }
