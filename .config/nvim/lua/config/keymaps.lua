d4626fa8f64aba7382b7d895b879016a7dfffe48
arghpy <subaandrei2@gmail.com>
2024-05-15 07:24:02 +0300

    add option to xdg-open under the cursor

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -19,9 +19,19 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Open terminal as a split below
--- TODO: improve it such that you can navigate more easily its output - e.g.: to get out of that stupid terminal mode
--- in order to copy paste
 vim.keymap.set('n', '<leader>t', ':split | terminal<CR>a', { desc = 'Open terminal as a split below' })
 
 -- Bind <ESC> in terminal mode do C-\_C-N
 vim.keymap.set('t', '<ESC>', '<C-\\><C-N>', { desc = 'Exit from terminal mode' })
+
+-- xdg-open under cursor (dilimited by space)
+vim.keymap.set('n', '<leader>o', function()
+    if vim.fn.executable("xdg-open") == 1 then
+        local word = vim.fn.expand("<cWORD>")
+        return { "xdg-open", word }
+    else
+        return nil, "no handler found"
+    end
+end
+    , { desc = 'xdg-open word under cursor' })
+
