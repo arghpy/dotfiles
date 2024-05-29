f454ac6a6c6694bb25700578ef214ccc572cf220
arghpy <subaandrei2@gmail.com>
2024-05-29 11:22:19 +0300

    [neovim] add formatters for rust and bash

--- .config/nvim/after/ftplugin/rust.lua
+++ .config/nvim/after/ftplugin/rust.lua
@@ -1,12 +1,7 @@
--- Make tab 2 spaces
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
-
 vim.api.nvim_create_autocmd({'BufWritePre'}, {
     callback = function()
         -- %!<cmd> is used to overwrite the whole buffer
         -- because shfmt just writes to stdout
-        vim.cmd("silent %!shfmt --indent 2 -sr -ci %")
+        vim.cmd("silent %!rustfmt --quiet --emit stdout %")
     end
 })
