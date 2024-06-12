1cf2c1d07897190579ba3cf606a5daa7e8666e2e
arghpy <arghpy@work>
2024-06-12 09:52:58 +0300

    [nvim] disable autoformatters

--- .config/nvim/after/ftplugin/sh.lua
+++ .config/nvim/after/ftplugin/sh.lua
@@ -3,10 +3,10 @@ vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 
-vim.api.nvim_create_autocmd({'BufWritePre'}, {
-    callback = function()
-        -- %!<cmd> is used to overwrite the whole buffer
-        -- because shfmt just writes to stdout
-        vim.cmd("silent %!shfmt --indent 2 -sr -ci %")
-    end
-})
+-- vim.api.nvim_create_autocmd({'BufWritePre'}, {
+--     callback = function()
+--         -- %!<cmd> is used to overwrite the whole buffer
+--         -- because shfmt just writes to stdout
+--         vim.cmd("silent %!shfmt --indent 2 -sr -ci %")
+--     end
+-- })
