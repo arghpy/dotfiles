4353997a8c7fa9eb964322abfd18e228ff3cd5cd
arghpy <subaandrei2@gmail.com>
2024-08-20 09:55:29 +0300

    [nvim] change theme

--- nvim/.config/nvim/lua/plugins/catppuccin-theme.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin-theme.lua
@@ -6,6 +6,6 @@ return {
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
         -- Load the colorscheme here.
-        vim.cmd.colorscheme 'catppuccin'
+        vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
 }
