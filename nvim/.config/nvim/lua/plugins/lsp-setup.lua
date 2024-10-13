378b700432b665c09d4a06acf12c998baf422207
arghpy <subaandrei2@gmail.com>
2024-10-13 11:00:12 +0300

    add completion for vim api

--- nvim/.config/nvim/lua/plugins/lsp-setup.lua
+++ nvim/.config/nvim/lua/plugins/lsp-setup.lua
@@ -42,6 +42,12 @@ return {
             lua_ls = {
                 settings = {
                     Lua = {
+                        workspace = {
+                          library = {
+                            unpack(vim.api.nvim_get_runtime_file('', true)),
+                          },
+                        },
+
                         completion = {
                             callSnippet = 'Replace',
                         },
