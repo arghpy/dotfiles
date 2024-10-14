8c2bef013fc3cae069ebcc12d1f6e4cfcf1d1a0d
arghpy <subaandrei2@gmail.com>
2024-10-14 09:08:38 +0300

    [nvim] fix shfmt

--- nvim/.config/nvim/after/ftplugin/sh.lua
+++ nvim/.config/nvim/after/ftplugin/sh.lua
@@ -4,6 +4,6 @@ vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
   callback = function()
     -- %!<cmd> is used to overwrite the whole buffer
     -- because shfmt just writes to stdout
-    vim.cmd("silent %!shfmt --indent 2 -sr -ci %")
+    vim.cmd("silent %!shfmt --indent 2 -sr -ci -")
   end
 })
