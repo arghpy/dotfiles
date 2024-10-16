74c8f24638b1acfe89853145de30b4e571be2ba0
arghpy <subaandrei2@gmail.com>
2024-10-16 08:49:42 +0300

    add keymaps for git signs

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -18,6 +18,18 @@ return {
       },
       numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
     }
+    -- Actions
+    vim.keymap.set('n', '<leader>hn', ':Gitsigns next_hunk<CR>', { desc = 'Git [H]unk [N]ext' })
+    vim.keymap.set('n', '<leader>hp', ':Gitsigns prev_hunk<CR>', { desc = 'Git [H]unk [P]rev' })
+    vim.keymap.set('n', '<leader>hs', ':Gitsigns stage_hunk<CR>', { desc = 'Git [H]unk [S]tage' })
+    vim.keymap.set('n', '<leader>hr', ':Gitsigns reset_hunk<CR>', { desc = 'Git [H]unk [R]eset' })
+    vim.keymap.set('n', '<leader>hu', ':Gitsigns undo_stage_hunk<CR>', { desc = 'Git [H]unk [U]ndo' })
+    vim.keymap.set('n', '<leader>hp', ':Gitsigns preview_hunk<CR>', { desc = 'Git [H]unk [P]review' })
+    vim.keymap.set('n', '<leader>bs', ':Gitsigns stage_buffer<CR>', { desc = 'Git [B]uffer [S]tage' })
+    vim.keymap.set('n', '<leader>br', ':Gitsigns reset_buffer<CR>', { desc = 'Git [B]uffer [R]eset' })
+    vim.keymap.set('n', '<leader>lb', ':Gitsigns blame_line<CR>', { desc = 'Git [L]ine [B]lame' })
+    vim.keymap.set('n', '<leader>dt', ':Gitsigns diffthis<CR>', { desc = 'Git [D]iff [T]his' })
+
     vim.cmd('highlight GitSignsAdd guifg=Green')
     vim.cmd('highlight GitSignsChange guifg=Yellow')
     vim.cmd('highlight GitSignsDelete guifg=Red')
