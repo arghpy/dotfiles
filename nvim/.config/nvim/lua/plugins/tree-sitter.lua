25fb549a7b5a5944757b5fdc5f34e0a055d928b6
arghpy <subaandrei2@gmail.com>
2024-12-18 05:26:51 +0200

    a revisit to tree-sitter

--- nvim/.config/nvim/lua/plugins/tree-sitter.lua
+++ nvim/.config/nvim/lua/plugins/tree-sitter.lua
@@ -1,21 +1,27 @@
+---@diagnostic disable: missing-fields, undefined-field
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require 'nvim-treesitter.configs'.setup {
+      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
 
-    configs.setup({
-      ensure_installed = { "lua" },
-      sync_install = false,
       -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
       auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-      incremental_selection = { enable = true },
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Using this option may slow down your editor
-      additional_vim_regex_highlighting = false,
-    })
+      highlight = {
+        enable = true,
+        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+      },
+    }
     -- Folding based on expression
     vim.opt.foldmethod = "expr"
     vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
