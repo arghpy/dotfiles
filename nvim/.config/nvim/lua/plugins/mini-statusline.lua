9f846be537ee7b928b8c6ddf97afd9cc45e1d737
arghpy <subaandrei2@gmail.com>
2024-12-19 08:15:24 +0200

    customize status line

--- nvim/.config/nvim/lua/plugins/mini-statusline.lua
+++ nvim/.config/nvim/lua/plugins/mini-statusline.lua
@@ -2,28 +2,28 @@ return {
   'echasnovski/mini.statusline',
   version = false,
 
-  active_content = function()
-    local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
-    local git           = MiniStatusline.section_git({ trunc_width = 40 })
-    local diff          = MiniStatusline.section_diff({ trunc_width = 75 })
-    local diagnostics   = MiniStatusline.section_diagnostics({ trunc_width = 75 })
-    local lsp           = MiniStatusline.section_lsp({ trunc_width = 75 })
-    local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
-    local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
-    local location      = MiniStatusline.section_location({ trunc_width = 75 })
-    local search        = MiniStatusline.section_searchcount({ trunc_width = 75 })
-
-    return MiniStatusline.combine_groups({
-      { hl = mode_hl,                  strings = { mode } },
-      { hl = 'MiniStatuslineDevinfo',  strings = { git, diff, diagnostics, lsp } },
-      '%<', -- Mark general truncate point
-      { hl = 'MiniStatuslineFilename', strings = { filename } },
-      '%=', -- End left alignment
-      { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
-      { hl = mode_hl,                  strings = { search, location } },
-    })
-  end,
   config = function()
-    require('mini.statusline').setup({ content = { active = active_content }})
+    require('mini.statusline').setup({
+      content = {
+        active = function()
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git           = MiniStatusline.section_git({ trunc_width = 40 })
+          local filename      = MiniStatusline.section_filename({ trunc_width = 140 })
+          local fileinfo      = vim.bo.filetype
+          -- local fileinfo      = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local location      = '%2l:%-2v'
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl,                  strings = { mode } },
+            { hl = 'MiniStatuslineDevinfo',  strings = { git } },
+            '%<', -- Mark general truncate point
+            { hl = 'MiniStatuslineFilename', strings = { filename } },
+            '%=', -- End left alignment
+            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+            { hl = mode_hl,                  strings = { location } },
+          })
+        end,
+      }
+    })
   end
 }
