fafaa62e7004b72365a012a4252a714069438cf7
arghpy <subaandrei2@gmail.com>
2024-12-19 11:19:53 +0200

    add another telescope keymap

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -28,6 +28,7 @@ return {
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>sg', builtin.live_multigrep, { desc = 'Telescope custom live_multigrep' })
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = 'Telescope diagnostics' })
       vim.keymap.set('n', '<leader>sn',
         function() require('telescope.builtin').find_files { cwd = vim.fn.stdpath("config") } end,
         { desc = 'Telescope help tags' })
