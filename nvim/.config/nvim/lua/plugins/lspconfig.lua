af306b916874d5169a2337f2645ea56ce9dc2dd5
arghpy <subaandrei2@gmail.com>
2025-07-15 08:33:14 +0300

    [nvim] add pylsp config

--- nvim/.config/nvim/lua/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lspconfig.lua
@@ -43,6 +43,19 @@ return {
             },
           }
         })
+      end,
+      ["pylsp"] = function ()
+        require("lspconfig").pylsp.setup({
+          settings = {
+            pylsp = {
+              plugins = {
+                pycodestyle = {
+                  maxLineLength = 120
+                }
+              }
+            }
+          }
+        })
       end
     }
     require('mason-tool-installer').setup {
