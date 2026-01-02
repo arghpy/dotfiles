c27a364e6673869c7c9de81e924e0598ff854730
arghpy <subaandrei2@gmail.com>
2026-01-02 12:09:17 +0200

    personalize rendering markdown

--- nvim/.config/nvim/lua/plugins/neowiki.lua
+++ nvim/.config/nvim/lua/plugins/neowiki.lua
@@ -1,10 +1,5 @@
 return {
   "echaya/neowiki.nvim",
-  opts = {
-    wiki_dirs = {
-      { name = "Notes", path = "~/notes" },
-    },
-  },
   dependencies = {
     {
       "MeanderingProgrammer/render-markdown.nvim",
@@ -15,4 +10,26 @@ return {
     { "<leader>wW", "<cmd>lua require('neowiki').open_wiki_floating()<cr>", desc = "Open Wiki in Floating Window" },
     { "<leader>wT", "<cmd>lua require('neowiki').open_wiki_new_tab()<cr>", desc = "Open Wiki in Tab" },
   },
+  config = function()
+    require('neowiki').setup({
+      wiki_dirs = {
+        { name = "Notes", path = "~/notes" },
+      },
+    })
+    require('render-markdown').setup({
+      sign = {
+        enabled = false,
+      },
+      heading = {
+        -- Width of the heading background.
+        -- | block | width of the heading text |
+        -- | full  | full width of the window  |
+        -- Can also be a list of the above values evaluated by `clamp(value, context.level)`.
+        width = "block",
+      },
+      code = {
+        width = "block",
+      },
+    })
+  end
 }
